61c288bf5f22f2a2b5fae68889c7974c533b129b
microsweet <weide.yang@hotmail.com>
2020-05-27 10:58:48 +0800

    edit wp change

--- wp-change.sh
+++ wp-change.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/ghibili
+feh --recursive --randomize --bg-fill ~/.config/wallpaper
 #feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
