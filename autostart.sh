ebef529c08c46f65ba0cb08b63256bb42e6a00ee
microsweet <weide.yang@hotmail.com>
2020-05-28 15:34:34 +0800

    when relogin do not start process

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-/bin/bash ~/scripts/dwm-status.sh &
-/bin/bash ~/scripts/wp-autochange.sh &
+proc_number=`ps -ef | grep -w dwm-status | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	/bin/bash ~/scripts/dwm-status.sh &
+fi
+
+proc_number=`ps -ef | grep -w wp-autochange | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	/bin/bash ~/scripts/wp-autochange.sh &
+else
+	kpid=`ps -ef | grep wp-autochange | grep -v grep | awk '{print $2}'`
+	kill -9 $kpid
+	/bin/bash ~/scripts/wp-autochange.sh &
+fi
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
-/bin/bash ~/scripts/tap-to-click.sh &
-/bin/bash ~/scripts/inverse-scroll.sh &
+
+proc_number=`ps -ef | grep -w picom | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	picom -b
+fi
+
+proc_number=`ps -ef | grep -w tap-to-click | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	/bin/bash ~/scripts/tap-to-click.sh &
+fi
+
+proc_number=`ps -ef | grep -w inverse-scroll | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	/bin/bash ~/scripts/inverse-scroll.sh &
+fi
 #/bin/bash ~/scripts/setxmodmap-colemak.sh &
+#network-manager-applet
 nm-applet &
 #xfce4-power-manager &
 #xfce4-volumed-pulse &
 #/bin/bash ~/scripts/run-mailsync.sh &
-fcitx &
-~/scripts/autostart_wait.sh &
+fcitx & 
+
+proc_number=`ps -ef | grep -w dwm-status | grep -v grep|wc -l`
+if [ $proc_number -le 0 ]; then
+	~/scripts/autostart_wait.sh &
+fi
