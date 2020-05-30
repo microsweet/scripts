e6d467599b4f1d2fe1d278751f8e36126835c5c2
Microsweet <weide.yang@hotmail.com>
2020-05-30 21:20:45 +0800

    add space between date and time

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -178,7 +178,7 @@ vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
 #xsetroot -name "   $(print_mem)M  $vel_recv  $vel_trans $(dwm_alsa) [ $(print_bat) ]$(show_record) $(print_date) "
-xsetroot -name "$(get_load) $(get_cputemp) $(get_ram) $(get_down_traffic) $(get_up_traffic) $(get_date)$(get_time) $(get_volume) "
+xsetroot -name "$(get_load) $(get_cputemp) $(get_ram) $(get_down_traffic) $(get_up_traffic) $(get_date) $(get_time) $(get_volume) "
 
 # Update old values to perform new calculations
 old_received_bytes=$received_bytes
